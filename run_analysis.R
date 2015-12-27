4687371be5ef35b517d892668ba1cc28495c3231
Rigzz <Flashrigzz@gmail.com>
2015-12-27 23:25:40 +0300

    Add - CodeBook

--- run_analysis.R
+++ run_analysis.R
@@ -18,7 +18,7 @@
 # Download data
 download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", 
               destfile = "./uci_har.zip")
-
+ 
 # Unzip archive
 unzip("./uci_har.zip")
 
